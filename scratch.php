1655d714ed4ce32e5166df2203dc099a833223f8
Andoly <andoly.cs@hotmail.com>
2019-06-10 15:21:40 -0300

    HTML Basic

---
+++ scratch.php
@@ -0,0 +1,30 @@
+<?php include_once "topo.php"; ?>
+<div>
+    <h1>Fundamentos de Web Design</h1>
+    <p>Aprenda HTML, CSS, Javascript</p>
+</div>
+</header>
+
+<main>
+    <section>
+        <div class="Card">
+            <img src="" alt="">
+            <p>Seletores </p>
+        </div>
+    </section>
+    <div class="Card">
+        <img src="" alt="">
+        <p>Seletores </p>
+    </div>
+    <div class="Card">
+        <img src="" alt="">
+        <p>Fontes </p>
+    </div>
+    <section>
+        <article>
+            <header>Título do article </header>
+            <p>P1</p>
+            <p>P2</p>
+        </article>
+    </section>
+    <?php include_once "footer.php"; ?>
---
+++ topo.php
@@ -0,0 +1,19 @@
+<html lang="pt-br">
+<head>
+    <title> pagina de exemplo estrutura basica </title>
+    <meta charset="utf-8">
+    <meta name="author" content="Andoly">
+    <meta name="description" content="descrição bacanuda">
+    <meta name="keywords" content="html5, tecnologia">
+    <link rel="stylesheet" href="CSS/estilo.css">
+</head>
+<body>
+<div id="Principal">
+    <header class="hero">
+        <nav>
+            <ul>
+                <li><a ref="index.html">Início</a></li>
+                <li><a ref="#">Projetos</a></li>
+                <li><a ref="index.html">Quem sou</a></li>
+            </ul>
+        </nav>
