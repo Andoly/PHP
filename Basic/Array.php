aa2352cfd1a6860720f7f66301af0c8f06e6222f
Andoly <andoly.cs@hotmail.com>
2019-08-22 01:03:33 -0300

    Working with array

--- Basic/Array.php
+++ Basic/Array.php
@@ -7,8 +7,28 @@ $y = $x;
 
 $animais = array("Gato", "Cachorro", "Coelho", "Macaco");
 #echo $animais[1]; imprime cachorro
+echo "Array de animais : <br />";
 for($i=0; $i<4; $i++ ){
     echo $animais[$i] ,"<br />";
 }
+echo "<br />";
+
+$nomes = array(0=>"Andoly", 1=> "Douglas", 2=> "Diony", 3=> "Maryana", 4=>"Thaynara");
+
+$sobrenomes = array(
+    0=>array(
+        0=>" Souza",
+        1=>" Viera",
+        2=>" Ribeiro",
+        3=>" Borges",
+        4=>" Santos"
+    ),
+    1=>"Array de Array"
+);
+echo "Array de nomes :  <br />";
+for($i=0; $i<5; $i++ ){
+    echo $nomes[$i] , $sobrenomes[0][$i],"<br />";
+}
+
 
 ?>
