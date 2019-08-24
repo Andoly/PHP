10d029d5f5ab559060d8eae71aa0af00a59824cd
Andoly <andoly.cs@hotmail.com>
2019-08-23 23:44:52 -0300

    print_r and var_dump

--- Basic/Array_associativo.php
+++ Basic/Array_associativo.php
@@ -1,18 +1,24 @@
 <?php
 $config = array(); // declaração do array
-$config["nome"] = "Andoly De Souza Borges";
-$config["linguagem"] = "PHP";
-$config["site"] =  "http://www.google.com.br";
+$config["Formação"] = "Curso: Ciências da Computação";
+$config["linguagem"] = "C / PHP / JAVA / Javascript / Flutter / PgAdmin4 / MySQL / HTML / CSS";
+$config["repositorio"] =  "https://github.com/Andoly/PHP";
 
-#echo $config["nome"], "<br/ >";
-#echo $config["linguagem"];
-
-//Representação ambigua
+echo $config["Formação"], "<br/>";
+echo $config["linguagem"], "<br/>";
+echo $config["repositorio"], "<br/>";
 
 $exemplo = array(
-    "nome" =>"Andoly De Souza Borges" ,
-    "linguagem" =>"PHP",
-    "site" =>  "http://www.google.com.br"
+    0=>array(
+        "nome" =>"Andoly",
+        "linguagem" =>"C / PHP / JAVA / Javascript / Flutter / PgAdmin4 / MySQL / HTML / CSS",
+    ),
+    1 =>"Framework : Laravel "
+
 );
-echo $exemplo["nome"];
+echo $exemplo["nome, <br/>"];
+echo "<br/> <br/> print_r : Desmembra a estruturação interna do array: <br/>";
+print_r($exemplo);
+echo "<br/> <br/> var_dump é um print_r que conta a quantidade arrays e sua quantidade de strings: <br/>";
+var_dump($exemplo);
 ?>
