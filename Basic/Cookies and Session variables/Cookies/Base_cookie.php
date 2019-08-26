376aafd150f4e93b0ad1e7c388e782a2e87ca8f6
Andoly <andoly.souza@discente.ufg.br>
2019-08-26 11:06:06 -0300

    Cookies

---
+++ Basic/Cookies and Session variables/Cookies/Base_cookie.php
@@ -0,0 +1,5 @@
+<?php
+$cor = "black";
+setcookie("cor",$cor, time()+360);
+?>
+<a href = "cookie.php"> Teste o cookie </a>
---
+++ Basic/Cookies and Session variables/Cookies/cookie.php
@@ -0,0 +1,6 @@
+<html>
+<!-- cookie são arquivos que ficam gravados no temporário do navegador, para agilizar a interação -->
+<head><title>Testando cookie</title></head>
+<body bgcolor ="<?=$_COOKIE['cor']?>">
+</body>
+</html>
