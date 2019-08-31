0c75adb866fb5443d75c36afe88c0df635da6039
Andoly <andoly.cs@hotmail.com>
2019-08-30 23:18:26 -0300

    store home page basic

---
+++ Basic/Loja/home.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+    <title>e-Commerce_Andoly</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+    <link rel="stylesheet" type="text/css" href="styles/fronted.css">
+    <link rel="stylesheet" type="text/css" href="styles/form.css">
+</head>
+    <body>
+        <div id="barrinha"></div>
+        <div id="estrutura">
+
+            <div id="topo">
+                <h1>Loja</h1>
+            </div>
+            <div class="clearfix"></div>
+
+            <div id="barra">
+                <div id="menu-horizontal">
+                    <ul>
+                        <li><a href="home.php">Home</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="conteudo">
+                <!-- Parte de conteudo da loja-->
+            </div>
+
+            <div class="clearfix"></div>
+            <div  id="footer">
+                <p>Loja Andoly - Todos os direitos reservados</p>
+            </div>
+    </body>
+</html>
